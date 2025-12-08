28441f186fb31499db80cf598b91e0db8d7ae762
Alfbom27 <alst2104@student.miun.se>
2025-12-08 10:04:17 +0100

    Update loss

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 
 class L1Loss(nn.Module):
     def __init__(self):
-        super(self).__init__()
+        super().__init__()
         self.loss = nn.L1Loss()
 
     def forward(self, pred, target):
