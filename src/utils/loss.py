89a5ac7c457afa16c3a9e84e8786dc2ae87175e3
Alfbom27 <alst2104@student.miun.se>
2025-12-15 11:53:44 +0100

    Aggregated loss from multiple layers

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -12,32 +12,30 @@ class L1Loss(nn.Module):
 
 
 class PerceptualLoss(nn.Module):
-    def __init__(self, shift=0):
+    def __init__(self):
         super().__init__()
 
-        self.model = models.vgg16(weights=models.VGG16_Weights.DEFAULT).features
+        self.layers = ['3', '8', '15', '22']
+        vgg_model = models.vgg16(weights=models.VGG16_Weights.DEFAULT).features
+        vgg_model.eval()
+        vgg_model.requires_grad_ = False
         self.device = "cuda:0" if torch.cuda.is_available() else "cpu"
-        self.model.to(self.device)
+        vgg_model.to(self.device)
+        self.vgg_layers = nn.ModuleList([vgg_model[:int(l)+1] for l in self.layers])
 
         self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                               std=[0.229, 0.224, 0.225])
-        self.shift = shift
-        self.training = False
-        self.model.eval()
-        self.model.requires_grad_ = False
-
-    def train(self, mode=True):
-        self.training = mode
 
     def forward(self, pred, target):
-        sep = pred.shape[0]
-        batch = torch.cat([pred, target])
-        if self.shift and self.training:
-            padded = nn.functional.pad(batch, [self.shift] * 4)
-            batch = transforms.RandomCrop(batch.shape[2:])(padded)
-        features = self.model(self.normalize(batch))
-        x_hat_features, x_features = features[sep:], features[:sep]
-        return nn.functional.mse_loss(x_hat_features, x_features)
+        pred = self.normalize(pred)
+        target = self.normalize(target)
+
+        loss = 0.0
+        for layer in self.vgg_layers:
+            pred_features = layer(pred)
+            target_features = layer(target)
+            loss += nn.functional.l1_loss(pred_features, target_features)
+        return loss
 
 
 class CombinedLoss(nn.Module):
